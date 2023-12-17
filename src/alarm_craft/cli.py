5ec0aabf9b6723141249ffb61e0ad9af6c03f92d
ryo-murai <murai.ry@gmail.com>
2023-12-18 00:05:06 +0900

    modify to upload as package

---
+++ src/alarm_craft/cli.py
@@ -0,0 +1,25 @@
+import argparse
+import logging
+import os
+
+from . import core
+
+def run():
+    logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
+    logger = logging.getLogger(__name__)
+
+    opts = core.CommandOpts(True, None, [], False)
+
+    argparser = argparse.ArgumentParser(description="AWS CloudWatch Alarm Craft")
+    argparser.add_argument("-f", "--no-confirm-changeset", action="store_false", dest="confirm_changeset")
+    argparser.add_argument("-n", "--notification-topic-arn", type=str, dest="notification_topic_arn", nargs="*", default=[])
+    argparser.add_argument("-c", "--config-file")
+    argparser.add_argument("-u", "--update-existing-alarms", action="store_true", dest="update_existing_alarms")
+    args = argparser.parse_args(namespace=opts)
+
+    logger.debug("commandline opts:%s", opts)
+
+    core.main(args)
+
+if __name__ == "__main__":
+    run()
