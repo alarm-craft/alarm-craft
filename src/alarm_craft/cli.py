9a7838dc6532ee86b571b842a700150a0f62867e
ryo-murai <murai.ry@gmail.com>
2023-12-21 22:52:16 +0900

    update black and isort

--- src/alarm_craft/cli.py
+++ src/alarm_craft/cli.py
@@ -14,7 +14,9 @@ def run() -> None:
 
     argparser = argparse.ArgumentParser(description="AWS CloudWatch Alarm Craft")
     argparser.add_argument("-f", "--no-confirm-changeset", action="store_false", dest="confirm_changeset")
-    argparser.add_argument("-n", "--notification-topic-arn", type=str, dest="notification_topic_arn", nargs="*", default=[])  # noqa: E501
+    argparser.add_argument(
+        "-n", "--notification-topic-arn", type=str, dest="notification_topic_arn", nargs="*", default=[]
+    )
     argparser.add_argument("-c", "--config-file")
     argparser.add_argument("-u", "--update-existing-alarms", action="store_true", dest="update_existing_alarms")
     args = argparser.parse_args(namespace=opts)
