cbf4e675811a5323464c4109e113d433f33e46dd
ryo-murai <murai.ry@gmail.com>
2024-02-04 15:24:13 +0900

    fix test config file path

--- tests/test_sample_yaml.py
+++ tests/test_sample_yaml.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 from alarm_craft import config_loader
 
 
 def test_sample_yaml():
     """Tests if sample yaml complies the config schema"""
-    config_loader.load("config-min-sample.yaml")
-    config_loader.load("config-sample.yaml")
+    curr_dir = Path(__file__).parent
+
+    config_loader.load(str(curr_dir.parent / "config-min-sample.yaml"))
+    config_loader.load(str(curr_dir.parent / "config-sample.yaml"))
